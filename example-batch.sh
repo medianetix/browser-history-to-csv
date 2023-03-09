fa6ab125d3ff1446d088dfcc03b1ab03193dd73a
Heiko Thurat <info@medianetix.de>
2023-03-09 14:09:17 +0100

    Update example-batch.sh

--- example-batch.sh
+++ example-batch.sh
@@ -6,14 +6,14 @@ STORAGE="/home/admin/stats/`date +'%Y-%m-%d'`";
 mkdir -p $STORAGE
 
 # user1: firefox and chrome
-python3 ./history.py -d /home/user1/.mozilla/firefox/zqvfbcc9.default-release -p user1-
-python3 ./history.py -d /home/user1/.config/google-chrome/Default -p user1-
+python3 ./history.py -b firefox -d /home/user1/.mozilla/firefox/zqvfbcc9.default-release -p user1-
+python3 ./history.py -b chrome -d /home/user1/.config/google-chrome/Default -p user1-
 
 # user2: firefox
-python3 ./history.py -d /home/user2/.mozilla/firefox/l4xwhv3z.default-release -p user2-
+python3 ./history.py -b firefox -d /home/user2/.mozilla/firefox/l4xwhv3z.default-release -p user2-
 
 # user3: chrome
-python3 ./history.py -d /home/user3/.config/google-chrome/Default -p user3-
+python3 ./history.py -b chrome -d /home/user3/.config/google-chrome/Default -p user3-
 
 # copy creates and copied files to storage dir
 cp /tmp/*.csv "$STORAGE/"
