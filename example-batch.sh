3f5e43f54881b8031799c631f472fc05762bfbea
Heiko Thurat <info@medianetix.de>
2023-03-09 13:24:12 +0100

    Update example-batch.sh

--- example-batch.sh
+++ example-batch.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-
 # execute as root, if accessing multiple users
-python3 ./places.py -d /home/user1/.mozilla/firefox/zqvfbcc9.default-release -p user1-
-python3 ./places.py -d /home/user2/.mozilla/firefox/l4xwhv3z.default-release -p user2-
-python3 ./places.py -d /home/user3/.mozilla/firefox/r5rdggfa.default-release -p user3-
 
 # make dir for storage
 STORAGE="/home/admin/stats/`date +'%Y-%m-%d'`";
 mkdir -p $STORAGE
+
+# user1: firefox and chrome
+python3 ./history.py -d /home/user1/.mozilla/firefox/zqvfbcc9.default-release -p user1-
+python3 ./history.py -d /home/user1/.config/google-chrome/Default -p user1
+
+# user2: firefox
+python3 ./history.py -d /home/user2/.mozilla/firefox/l4xwhv3z.default-release -p user2-
+
+# user3: chrome
+python3 ./history.py -d /home/user3/.config/google-chrome/Default -p user3-
 
 # copy creates and copied files to storage dir
 cp /tmp/*.csv "$STORAGE/"
